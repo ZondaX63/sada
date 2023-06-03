1fe5081ddc14a9fabf8728325db13fb6911775ff
ZondaX <61884303+ZondaX63@users.noreply.github.com>
2023-06-03 13:11:49 +0300

    Add files via upload

---
+++ sdsa.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot olarak giriş yapıldı: {bot.user.name}')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member.id == 707329534600675329:
+        if before.channel != after.channel:
+            if after.channel is not None:  # Kullanıcı bir ses kanalına katıldıysa
+                current_channel_id = 1113896222852395028
+                current_channel = bot.get_channel(current_channel_id)
+                target_channel_id = 1114488605755326514  # Değiştirmeniz gereken kısım
+                target_channel = bot.get_channel(target_channel_id)
+                if current_channel:
+                    await member.move_to(target_channel)
+
+bot.run('MTExNDE0NjQ2ODUxMzcyMjM4OQ.GtgOGF.0ytY_xLmOzIad27H3H-jQZVjlQ5VQv4yDpXmmE')  # Değiştirmeniz gereken kısım
